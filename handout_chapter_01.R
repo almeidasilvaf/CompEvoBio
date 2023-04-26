e2542c88870b4960b54cd091e227cd2b4ace8814
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2023-04-26 20:16:29 +0200

    Added code handout for chapter 1

---
+++ handout_chapter_01.R
@@ -0,0 +1,112 @@
+
+
+# Creating objects ====
+
+
+
+
+# Functions and arguments ====
+
+
+
+
+## Exercises ====
+
+#' The genome of the plant Arabidopsis thaliana contains 33768 genes, 
+#' of which 27655 are protein-coding and 6113 encode non-coding RNAs. 
+#' Calculate the percentage of coding and non-coding genes in this genome 
+#' using the following steps:
+#' 
+#' 
+#' - Create a variable named `ath_genes` that stores the numeric value 33768.
+#' 
+#' - Create 2 objects named `n_coding` and `n_noncoding` that store 
+#' the numeric values 27655 and 6113, respectively.
+#' 
+#' - Create 2 objects named `perc_coding` and `perc_noncoding` by dividing 
+#' `n_coding` and `n_noncoding` by `ath_genes`.
+#' 
+#' - Update the objects by multiplying their contents by 100 (to represent 
+#' in percentages, from 0 to 100) and rounding the percentages to 1 
+#' significant digit only.
+#' 
+
+
+
+
+
+# Vectors ====
+
+
+
+
+
+
+
+# Data frames ====
+
+
+
+
+
+
+## Exercises ====
+
+#' The plants Brussels sprout, grapevine and apple belong to the 
+#' families Brassicaceae, Vitaceae, and Rosaceae. 
+#' Create a data frame named `species_families` with 2 columns named 
+#' `species` and `family` representing such information.
+#' 
+
+
+
+
+#' When you install R, it already comes with some example data sets. 
+#' One of them is `airquality`, a data frame containing New York air 
+#' quality measurements from May to September 1973. Inspect this data frame 
+#' and answer the following questions:
+#'     
+#' 1. How many rows are there?
+#' 2. How many columns are there?
+#' 3. What are the column names?
+#' 4. What are the classes of each column?
+
+
+
+
+
+
+
+# Indexing and subsetting vectors
+
+
+
+
+
+
+
+
+# Indexing and subsetting data frames
+
+
+
+
+
+
+
+## Exercises ====
+
+#' Subset the `airquality` data set to answer the following questions:
+#' 
+#' 1. How many days (rows) observations have ozone levels >40 ppb?
+#' 2. How many days have wind speed between 10 and 15 mph?
+#' 3. What are the minimum and maximum temperature?
+#' 4. How many days have solar radiation <100 Langleys?
+ 
+
+
+
+
+
+
+
